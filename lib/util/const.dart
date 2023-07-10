843eb430b6233e6280d2c20f984c48dcc3251bd7
AhmedYahiaoui <ahmed.yahyaoui.2@esprit.tn>
2023-07-10 02:09:50 +0200

    Push the code

---
+++ lib/util/const.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+class Constants{
+
+  static String appName = "Restaurant App UI KIT";
+
+  static Color lightPrimary = Color(0xfffcfcff);
+  static Color darkPrimary = Colors.black;
+  static Color lightAccent = Colors.red;
+  static Color darkAccent = Colors.red[400];
+  static Color lightBG = Color(0xfffcfcff);
+  static Color darkBG = Colors.black;
+  static Color ratingBG = Colors.yellow[600];
+
+  static ThemeData lightTheme = ThemeData(
+    backgroundColor: lightBG,
+    primaryColor: lightPrimary,
+    accentColor:  lightAccent,
+    cursorColor: lightAccent,
+    scaffoldBackgroundColor: lightBG,
+    appBarTheme: AppBarTheme(
+      ),
+    ),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    backgroundColor: darkBG,
+    primaryColor: darkPrimary,
+    accentColor: darkAccent,
+    scaffoldBackgroundColor: darkBG,
+    cursorColor: darkAccent,
+    appBarTheme: AppBarTheme(
+      ),
+    ),
+  );
+
+
+}
